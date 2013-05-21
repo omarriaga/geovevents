19a1802b09deee3cb33db65d6d6d783078045d0e
unknown <omarrc9123@gmail.com>
2013-05-20 23:13:03 -0500

    cambios

--- app/controllers/api/push_controller.rb
+++ app/controllers/api/push_controller.rb
@@ -2,16 +2,19 @@ class Api::PushController < ApplicationController
 
 	before_filter :verify_application
 	def create
-
-		
+		respond_to do |format|
+			format.any{
+				render_errors ["invalid format just JSON"]
+			}
+			format.json{}
+		end
 	end
 
 private
 	def verify_application
 		@application = Application.find_by_application_token(params[:application_token])
 		if @application.nil?
-			
+			render_errors(["invalid application"])
 		end
-
 	end
 end
